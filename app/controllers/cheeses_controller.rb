bec45d1bf365082d2caef1d012ef9ca3c31d7889
Jobwawesh <jobnjunge15@gmail.com>
2023-03-14 12:45:25 +0300

    Completed Assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,6 +3,8 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
+      render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+
     render json: cheeses
   end
 
